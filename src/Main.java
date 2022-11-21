6b4b8f75de591974424c8497e478656459db454d
HarshaSalih <harshasalih2000@gmail.com>
2022-11-21 12:48:47 +0530

    largest commits

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Choose an option");
+        int option;
+        do
+            {
+
+                System.out.println("1.Find largest\n2.Find smallest\n3.Check prime or not\n4.Check even or not\n5.Find reverse\n6.Exit");
+                System.out.println("Choose an option");
+                Scanner sc=new Scanner(System.in);
+                option=sc.nextInt();
+                switch (option)
+                {
+                    case 1:
+                        System.out.println("Enter three numbers");
+                        int num1=sc.nextInt();
+                        int num2=sc.nextInt();
+                        int num3=sc.nextInt();
+                        if(num1>num2 && num1>num3)
+                        {
+                            System.out.println("Largest is "+num1);
+                        }
+                        else if(num2>num3)
+                        {
+                            System.out.println("Largest is "+num2);
+                        }
+                        else
+                        {
+                            System.out.println("Largest is "+num3);
+                        }
+                }
+
+
+        }
+        while(option!=5);
+
     }
 }
